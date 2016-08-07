4611b1d1f8d2bde78cd7027a7e003b34ad692db5
Maria <maria.arinbjarnar@gmail.com>
2016-08-07 21:50:15 +0100

    Weather api

--- api.py
+++ api.py
@@ -1,12 +1,19 @@
-
+import constants
 import requests
 
 class Mailgun():
     def send(self, to, subject, msg):
         return requests.post(
             "https://api.mailgun.net/v3/sandbox03a400ad1bbd421da31d9dcd3360f748.mailgun.org/messages",
-            auth=("api", "key-b6a4d8ce526a19f0013a0c9dba7ae48d"),
+            auth=("api", constants.MAIL_KEY),
             data={"from": "Mailgun Sandbox <postmaster@sandbox03a400ad1bbd421da31d9dcd3360f748.mailgun.org>",
                   "to": to,
                   "subject": subject,
                   "text": msg})
+
+class Weather():
+    def weatherByCity(self, name):
+        endpoint = "http://api.openweathermap.org/data/2.5/weather"
+        payload = {"q": name, "appid": constants.WHEATHER_KEY}
+        print endpoint, payload
+        return requests.get(endpoint, params=payload)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from flask import Flask
 from flask import render_template
 from flask import request
-from api import Mailgun
+from api import *
 
 app = Flask(__name__)
 
@@ -28,11 +30,27 @@ def email():
     subject = "Hello Maria",
     msg = "Congratulations Maria, you just sent an email with Mailgun!  You are truly awesome!  " \
             "You can see a record of this email in your logs: https://mailgun.com/cp/log .  " \
-            "You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free."
+            "You can send up to 300 emails/day from this sandbox server. "
     m = Mailgun()
     result = m.send(to,subject,msg)
+    return render_template("index.html", email=result.status_code)
+
+
+@app.route("/weather", methods=['POST'])
+def weather():
+    w = Weather()
+    form_data = request.form
+    name = form_data['city']
+    result = w.weatherByCity(name).json()
     print result
-    return render_template("index.html", email=result)
+    temp = result['main']['temp']
+    city = result['name']
+    country = result['sys']['country']
+    weather = result['weather'][0]['main']
+    print temp, city, country, weather
+    msg = u"It's {}ºC in {} {}, and the sky is {} ".format(temp, city, country, weather)
+    return render_template("index.html", weather=msg)
+
 
 if __name__ == "__main__":
     app.run()
