5c0c36ca0698a4093d4c2fba6303b22189e9b72d
Skippla <maria.arinbjarnar@gmail.com>
2016-08-07 16:43:40 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -34,16 +34,5 @@ def email():
     print result
     return render_template("index.html", email=result)
 
-# class Mailgun():
-#     def send(self, to, subject, msg):
-#         return requests.post(
-#             "https://api.mailgun.net/v3/sandbox03a400ad1bbd421da31d9dcd3360f748.mailgun.org/messages",
-#             auth=("api", "key-b6a4d8ce526a19f0013a0c9dba7ae48d"),
-#             data={"from": "Mailgun Sandbox <postmaster@sandbox03a400ad1bbd421da31d9dcd3360f748.mailgun.org>",
-#                   "to": to,
-#                   "subject": subject,
-#                   "text": msg})
-
-
 if __name__ == "__main__":
     app.run()
