7c76879e7e47332c640bd3f907a106b18855b274
Cristina <cristina.pascalau@gmail.com>
2017-10-11 21:51:14 +0100

    Changes to old template. Incorporated the APIs to work with the new format.

--- main.py
+++ main.py
@@ -7,15 +7,18 @@ from flask import request
 from api import *
 
 template_dir = os.path.dirname(os.path.abspath(__file__))
-app = Flask(__name__, template_folder=template_dir)
+app = Flask(__name__,
+            template_folder=template_dir,
+            static_url_path='',
+            static_folder=template_dir)
 import json
 from watson_developer_cloud import ConversationV1
 
 CONVERSATION = ConversationV1(
-        username='825f6457-82d9-4d80-9073-9f6cf7d9947d',
-        password='MqMWQu3hwwF3',
-        version='2017-08-05')
-WORKSPACE_ID = 'ac827c64-04d4-47b7-b2c5-4d8a61349dd7'
+        username='7859a24d-768c-46f3-bf57-d13a8a61b975',
+        password='3X4GfI4qnbOB',
+        version='2017-10-01')
+WORKSPACE_ID = '27511189-bce7-48ad-85b9-ff79b5d9d4ff'
 
     
 @app.route("/")
@@ -76,8 +79,6 @@ def twitter():
     return render_template("index.html", tweets=tweets)
 
 
-
-
 @app.route("/foursquare", methods=['POST'])
 def foursquare():
     f = Foursquare()
@@ -99,7 +100,7 @@ def uber_product():
     latitude = form_data['latitude']
     longitude = form_data['longitude']
     products = u.getProducts(latitude, longitude)
-    return render_template("index.html", products=products)
+    return render_template("uberPage.html", products=products)
 
 @app.route("/aftership", methods=['POST'])
 def aftership_Create_Shipment():
@@ -118,15 +119,18 @@ def speak(message, context):
 
 
 @app.route("/watson", methods=['POST'])
-@app.route("/game", methods=['POST'])
 def conversation():
     form_data = request.form
     message = form_data.get('answer', '')
-    context = json.loads(form_data.get('context', "{}"))
+    try:
+        context = json.loads(form_data.get('context', "{}"))
+    except ValueError:
+        # No context yet, we create it empty
+        context = {}
     response, context = speak(message, context)
-    return render_template("game.html", response=response, context=json.dumps(context))
+    return render_template("watson.html", response=response, context=json.dumps(context))
 
 
 #app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
 if __name__ == "__main__":
-    app.run()
+    app.run(port=65438)
